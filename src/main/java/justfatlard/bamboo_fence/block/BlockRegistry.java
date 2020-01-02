3eb09f5e9d22387d1e4bbb95de0a97d8c516ff3b
Chase Whitfield <fatlard1993@gmail.com>
2020-01-02 00:59:50 -0800

    fix hardness and resistance

--- src/main/java/justfatlard/bamboo_fence/block/BlockRegistry.java
+++ src/main/java/justfatlard/bamboo_fence/block/BlockRegistry.java
@@ -8,9 +8,9 @@ import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
     private static final String MODID = "bamboo-fence-justfatlard";
-    public static final BlockBambooFence BAMBOO_FENCE_BLOCK = new BlockBambooFence(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 3.0F).build());
-    public static final BlockBambooFenceGate BAMBOO_FENCE_GATE_BLOCK = new BlockBambooFenceGate(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 3.0F).build());
-    public static final BlockBambooFenceWall BAMBOO_FENCE_WALL_BLOCK = new BlockBambooFenceWall(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 3.0F).build());
+    public static final BlockBambooFence BAMBOO_FENCE_BLOCK = new BlockBambooFence(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 15F).build());
+    public static final BlockBambooFenceGate BAMBOO_FENCE_GATE_BLOCK = new BlockBambooFenceGate(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 15F).build());
+    public static final BlockBambooFenceWall BAMBOO_FENCE_WALL_BLOCK = new BlockBambooFenceWall(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 15F).build());
 
     public static void registerBlocks(){
         Registry.register(Registry.BLOCK, new Identifier(MODID, "bamboo_fence_gate"), BAMBOO_FENCE_GATE_BLOCK);
