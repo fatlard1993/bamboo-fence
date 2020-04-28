283cab7e2576eda658d615e77539df176811cd8e
fatlard1993 <fatlard1993>
2020-04-27 17:23:34 -0700

    register fuels

--- src/main/java/justfatlard/bamboo_fence/block/BlockRegistry.java
+++ src/main/java/justfatlard/bamboo_fence/block/BlockRegistry.java
@@ -1,20 +1,25 @@
 package justfatlard.bamboo_fence.block;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.impl.content.registry.FuelRegistryImpl;
 import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
-    private static final String MODID = "bamboo-fence-justfatlard";
+    private static final String MOD_ID = "bamboo-fence-justfatlard";
     public static final BlockBambooFence BAMBOO_FENCE_BLOCK = new BlockBambooFence(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 15F).build());
     public static final BlockBambooFenceGate BAMBOO_FENCE_GATE_BLOCK = new BlockBambooFenceGate(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 15F).build());
     public static final BlockBambooFenceWall BAMBOO_FENCE_WALL_BLOCK = new BlockBambooFenceWall(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(2.0F, 15F).build());
 
     public static void registerBlocks(){
-			Registry.register(Registry.BLOCK, new Identifier(MODID, "bamboo_fence_gate"), BAMBOO_FENCE_GATE_BLOCK);
-			Registry.register(Registry.BLOCK, new Identifier(MODID, "bamboo_fence"), BAMBOO_FENCE_BLOCK);
-			Registry.register(Registry.BLOCK, new Identifier(MODID, "bamboo_fence_wall"), BAMBOO_FENCE_WALL_BLOCK);
+			Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "bamboo_fence"), BAMBOO_FENCE_BLOCK);
+			Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "bamboo_fence_gate"), BAMBOO_FENCE_GATE_BLOCK);
+			Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "bamboo_fence_wall"), BAMBOO_FENCE_WALL_BLOCK);
+
+			FuelRegistryImpl.INSTANCE.add(BAMBOO_FENCE_BLOCK, 300);
+			FuelRegistryImpl.INSTANCE.add(BAMBOO_FENCE_GATE_BLOCK, 300);
+			FuelRegistryImpl.INSTANCE.add(BAMBOO_FENCE_WALL_BLOCK, 300);
     }
 }
